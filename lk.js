7b2d019ef66ceb2a84b594fce2495045b187b7d8
shahid <shahidshams2011@gmail.com>
2019-11-14 22:44:37 +0000

    new file

---
+++ lk.js
@@ -0,0 +1,8 @@
+var answer1 = "It's alright";
+var answer2 = "He is called 'Johnny'";
+var answer3 = 'He is called "Johnny"';
+
+document.getElementById("demo").innerHTML =
+answer1 + "<br>" + 
+answer2 + "<br>" + 
+answer3;
